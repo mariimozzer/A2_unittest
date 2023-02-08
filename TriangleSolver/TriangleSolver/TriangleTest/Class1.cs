8788d9d727805bc79bf8a9f2f8325b3f91819453
mariimozzer <102536415+mariimozzer@users.noreply.github.com>
2023-02-08 17:13:43 -0500

    Scalene

--- TriangleSolver/TriangleSolver/TriangleTest/Class1.cs
+++ TriangleSolver/TriangleSolver/TriangleTest/Class1.cs
@@ -12,8 +12,10 @@ namespace TriangleTest
     [TestFixture]
     public class Class1
     {
+        //EQUILATERAL TESTING
+
         [Test]
-        public void IsEquilateral_input222_true()
+        public void IsEquilateral_input2_2_2_true()
         {
             //Arrange
             string expected = "Based on all sides being equal, the type of triangle is an EQUILATERAL";
@@ -27,8 +29,10 @@ namespace TriangleTest
             Assert.AreEqual(expected, actual);
         }
 
+        //ISOSCELES TESTING
+
         [Test]
-        public void IsIsosceles_input232_true()
+        public void IsIsosceles_input2_3_2_true()
         {
             //Arrange
             string expected = "Based on two sides being equal, the type of triangle is an ISOSCELES";
@@ -42,9 +46,8 @@ namespace TriangleTest
             Assert.AreEqual(expected, actual);
         }
 
-
         [Test]
-        public void IsIsosceles_input223_true()
+        public void IsIsosceles_input2_2_3_true()
         {
             //Arrange
             string expected = "Based on two sides being equal, the type of triangle is an ISOSCELES";
@@ -59,7 +62,7 @@ namespace TriangleTest
         }
 
         [Test]
-        public void IsIsosceles_input322_true()
+        public void IsIsosceles_input3_2_2_true()
         {
             //Arrange
             string expected = "Based on two sides being equal, the type of triangle is an ISOSCELES";
@@ -68,6 +71,83 @@ namespace TriangleTest
             //Act
             Triangle tri = new Triangle();
             actual = tri.AnalyzeTriangle(3, 2, 2);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //SCALENE TESTING
+
+        [Test]
+        public void IsScalene_input7_4_5_true()
+        {
+            //Arrange
+            string expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+            string actual;
+
+            //Act
+            Triangle tri = new Triangle();
+            actual = tri.AnalyzeTriangle(7,4,5);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IsScalene_input100_140_120_true()
+        {
+            //Arrange
+            string expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+            string actual;
+
+            //Act
+            Triangle tri = new Triangle();
+            actual = tri.AnalyzeTriangle(100,140,120);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IsScalene_input4_5_6_true()
+        {
+            //Arrange
+            string expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+            string actual;
+
+            //Act
+            Triangle tri = new Triangle();
+            actual = tri.AnalyzeTriangle(4,5,6);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IsScalene_input8_7_10_true()
+        {
+            //Arrange
+            string expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+            string actual;
+
+            //Act
+            Triangle tri = new Triangle();
+            actual = tri.AnalyzeTriangle(8,7,10);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IsScalene_input6_5_4_true()
+        {
+            //Arrange
+            string expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+            string actual;
+
+            //Act
+            Triangle tri = new Triangle();
+            actual = tri.AnalyzeTriangle(6,5,4);
 
             //Assert
             Assert.AreEqual(expected, actual);
